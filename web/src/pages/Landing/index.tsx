d491b3a12c91682cab67bc7896fe9bcf9a181df1
Mateus M <mateus.mlo95@gmail.com>
2020-08-05 15:03:30 -0300

    finish desktop front-end and integrate back-end

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import api from '../../services/api'
 
 //images
 import logoImg from '../../assets/images/logo.svg'
@@ -14,6 +15,16 @@ import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
 import './styles.css'
 
 function Landing() {
+	const [totalConnections, setTotalConnections] = useState(0)
+
+	useEffect(() => {
+		api.get('/connections').then((res) => {
+			const { total } = res.data
+
+			setTotalConnections(total)
+		})
+	}, [])
+
 	return (
 		<div id="page-landing">
 			<div id="page-landing-content" className="container">
@@ -41,7 +52,7 @@ function Landing() {
 				</div>
 
 				<span className="total-connections">
-					Total de 200 conexões já realizadas
+					Total de {totalConnections} conexões já realizadas
 					<img src={purpleHeartIcon} alt="Coração roxo" />
 				</span>
 			</div>
